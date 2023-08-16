fc7f7f993c8a15847e5ee06cb2865b0f21b4fe9b
Do Lee <Do.Lee@swgas.com>
2023-08-16 08:13:09 -0700

    app firebase

--- GroceryApp/app/index.js
+++ GroceryApp/app/index.js
@@ -1,4 +1,20 @@
-/*
-Challenge:
-Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
-*/
+//import { testing } from "../functions";
+import { testing } from "/GroceryApp/app/functions.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+const appSettings = {
+    databaseURL: "https://playground-e9cce-default-rtdb.firebaseio.com/"
+}
+//console.log(testing())
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+console.log("what");
+console.log(app)
+const inputfieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button")
+console.log("Hellodfdfsd")
+addButtonEl.addEventListener("click", function(){
+    
+    console.log(inputfieldEl.value);
+    console.log("Hello");
+})
